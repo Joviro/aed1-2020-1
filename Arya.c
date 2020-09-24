b9655b91dad78bb99df8b2ac73f31dfad647abe8
Joviro <71459476+Joviro@users.noreply.github.com>
2020-09-24 15:57:32 -0300

    Add files via upload
    Oi, estou mandando aqui mermao

--- Arya.c
+++ Arya.c
@@ -1,9 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef int TIPOCHAVE;
+
+typedef struct{
+
+	TIPOCHAVE chave;
+
+} NOME;
+
+typedef struct aux{
+
+	NOME ini;
+	struct aux* proximo;
+
+} INIMIGO;
+
+typedef struct{
+
+	INIMIGO* inicio;
+
+}LISTA;
+
+
+void insere(LISTA* lista, int inimigo){}
+void exclui(){}
+void quantidade_intervalo(){}
+void quantidade(){}
+
+
+
 int main (){
 
-    
+	char menu, inimigos_loop;
+	int inimigo,loop = 1;
+	menu = 'Z';
+
+	LISTA* lista;
+	lista->inicio = NULL;
+
+	printf("Quantos inimigos teremos inicialmente?\n");
+	scanf("%i", &inimigos_loop);
+
+	//Insere os primeiros elementos:
+	for(int x = 0; x < inimigos_loop; x++){
+        printf("Qual o valor do inimigo numero %i: \n",loop);
+        scanf("%i", inimigo);
+        insere(lista, inimigo);
+        loop++;
+	}
+
+	for(int x = 0; x < inimigos_loop; x ++){
+
+        insere(lista, inimigo);
+
+	}
+
+	while(menu != 'F'){
+
+
+
+	}
+
+    printf("fim\n");
 
     return 0;
 }
