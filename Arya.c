5c6ffeead79f35c7301e8eb55aba0c62cde699e6
Joviro <71459476+Joviro@users.noreply.github.com>
2020-09-28 15:58:29 -0300

    Add files via upload

--- Arya.c
+++ Arya.c
@@ -19,51 +19,51 @@ void iniciarLista (LISTA *lista){
     lista->inicio = NULL;
 }
 
-void insere_primeiros(lista, inimigo){
+void insere_primeiros(LISTA* lista, int inimigo){
 
     LISTA* end = lista->inicio;
     if(end == NULL){
-    
+
       INIMIGO* inimigx = (INIMIGO*) malloc (sizeof(INIMIGO));
-      lista->inicio = ini;	     
+      lista->inicio = inimigx;
       inimigx->ini.chave = inimigo;
-      inimigx->prox = NULL;	    	    
+      inimigx->proximo = NULL;
     }
    else{
     INIMIGO* inimigx;
-    inimigx = lista->inicio;	   
+    inimigx = lista->inicio;
     while(inimigx != NULL){
 
-         inimigx = inimigx->prox;  
-    
+         inimigx = inimigx->proximo;
+
     }
    INIMIGO* inimigox = (INIMIGO*) malloc (sizeof(INIMIGO));
    inimigx->proximo = inimigox;
    inimigox->proximo = NULL;
-   iminigox->ini.TIPOCHAVE = inimigo;	   
-	   
-  } 	
+   inimigox->ini.chave = inimigo;
+
+  }
 }
 
 void insere(LISTA* lista, int chave, int anterior){
 
-     Lista* end = lista->inicio;
+     LISTA* end = lista->inicio;
      while(end != NULL){
-     
-     }	
+
+     }
 
 }
 
-void remove(LISTA* lista, TIPOCHAVE c){
+void remover(LISTA* lista, TIPOCHAVE chave){
 
 }
 
-void quantidade_intervalo(Lista* lista, TIPOCHAVE a, TIPOCHAVE b){
+void quantidade_intervalo(LISTA* lista, TIPOCHAVE a, TIPOCHAVE b){
 
 }
 
 void quantidade_total(LISTA* lista){
-	
+
     INIMIGO *end = lista->inicio;
     int tam = 0;
     printf ("lista ");
@@ -99,26 +99,26 @@ int main (){
 	for(int x = 0; x < inimigos_loop; x++){
         printf("Qual o valor do inimigo numero %i: \n",loop);
         scanf("%i", &inimigo);
-        insere_primeiros(lista, inimigo);	
+        insere_primeiros(lista, inimigo);
         loop++;
         }
 
 	while(menu != 'F'){
-	printf("Bem vindo ao menu:\nDigite I para adicionar um elemento\nDigite R para remover um elemento\nDigite C para mostrar os valores em um intervalo de elementos\nDigite M para mostrar todos os elementos da fila\nDigite F para finalizar o programa\n\n");		
+	printf("Bem vindo ao menu:\nDigite I para adicionar um elemento\nDigite R para remover um elemento\nDigite C para mostrar os valores em um intervalo de elementos\nDigite M para mostrar todos os elementos da fila\nDigite F para finalizar o programa\n\n");
 	scanf("%c", &menu);
 	if(menu == 'I'){
-	
-	   printf("Digite a chave do elemento e qual serÃ¡ o elemento anterior a ele: \n");
-	   int chave, anterior;		
-	   insere(lista, chave, anterior);		
+
+	   printf("Digite a chave do elemento e qual será o elemento anterior a ele: \n");
+	   int chave, anterior;
+	   insere(lista, chave, anterior);
 	}
 	else if(menu == 'R');
-	else if(menu == 'C');	
+	else if(menu == 'C');
 	else if(menu == 'M');
-	else printf("NÃ£o existe essa operaÃ§Ã£o. Tente novamente!\n");
-	
+	else printf("Não existe essa operação. Tente novamente!\n");
+
 	}
-	
+
     printf("fim\n");
 
     return 0;
